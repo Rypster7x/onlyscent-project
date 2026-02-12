624d422de2f52881e57a16c015cca01e8b436f7e
Rypster7x <s.lopezhuamantinco@sintlucasedu.nl>
2026-02-12 11:50:56 +0100

    Making progress
    q

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const btn = document.querySelector('.btnFilter');
+    btn.addEventListener('click', () => {
+        window.open('https://fashion.jeanpaulgaultier.com/en-nl?_gl=1*1n3idgz*_gcl_aw*R0NMLjE3NzA4OTI0OTMuQ2owS0NRaUE0cHZNQmhEWUFSSXNBR2Znd3Z4SndtOFdhRHVPRjM4cXkzMHlnWGxNRk5PbWtzcVBoV1puYXBkeUt1ZEtrcFNMOGtYdmZsTWFBa0xSRUFMd193Y0I.*_gcl_au*MTkzMjY4NzY1OS4xNzcwMTI4OTYw*_ga*NDU1MDI3ODkzLjE3NzAxMjg5NTk.*_ga_9X68GSCJWV*czE3NzA4OTEwMDkkbzUkZzEkdDE3NzA4OTI0OTIkajU3JGwwJGgw', '_blank');
+    });
+});
